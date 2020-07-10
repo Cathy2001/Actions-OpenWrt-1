e2182f60f904ec116528cbe8dd6568e7e2084726
yochengcheung <67940477+yochengcheung@users.noreply.github.com>
2020-07-11 00:50:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,3 +14,4 @@
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git Adguard https://github.com/AdguardTeam/AdGuardHome' feeds.conf.default
